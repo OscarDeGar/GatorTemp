78c4bef2bfb021644a53e37a2daed4cd820a7e0b
jes-bro <91238623+jes-bro@users.noreply.github.com>
2022-03-09 11:25:21 -0500

    Add read_sonar arduino sketch

---
+++ Teensy/read_sonar.ino
@@ -0,0 +1,26 @@
+#define sensorPin A10
+ 
+int distance = 0;
+ 
+void setup() {
+ Serial.begin(9600);
+}
+ 
+void read_sensor() {
+  int start_time = micros();
+  distance = analogRead(sensorPin) * 1;
+  int time_to_completion = micros() - start_time;
+  Serial.println(time_to_completion);
+}
+ 
+void print_data() {
+ Serial.print("distance = ");
+ Serial.print(distance);
+ Serial.println(" cm");
+}
+ 
+void loop() {
+ read_sensor();
+ print_data();
+ delay(1000);
+}
