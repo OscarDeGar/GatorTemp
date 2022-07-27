6ef4647250d7960497c45a86c9baa1ef5cc1dda2
Cbabe <78317842+Cbabe@users.noreply.github.com>
2022-07-27 10:43:13 -0400

    GPS navi

--- Gator Summer/ACT/actMotors.m
+++
@@ -1,7 +0,0 @@
-function actMotors(Mega, steerAngle, throttle, brake)
-%%% Mojave motor control via serial comms
- % INPUTS: steerAngle(int) - angle to steer towards
- %         throttle(int) - scalar 0 to 1 to press gas pedal
- %         brake(int) - scalar 0 to 1 to press brake pedal
- 
-end
