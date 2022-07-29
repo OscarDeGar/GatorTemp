81013cfdfd7e4781be366ca21fa6a9367bf65078
robo-gator <robogator@outlook.com>
2022-07-29 10:32:38 -0400

    Merge Issues

--- Gator Summer/SENSE/senseGPS.m
+++ Gator Summer/SENSE/senseGPS.m
@@ -2,23 +2,36 @@ function gpsData = senseGPS(obj)
 %%% Read GPS data from U-Center output file
  % OUTPUTS: gpsData(struct) - longitude, latitude data
     %%$ Read Data
-    %fileID = fopen("C:\Users\Skull\Documents\GatorTemp\RTK_GPS\GPS_1_Data.ubx");
+    parserObj = nmeaParser("MessageId","RMC");
     format long
     fileID = fopen("C:\Users\Skull\Documents\GatorTemp\RTK_GPS\GPS_Test_Parkinglot_Left.ubx",'r');
     corrections=fscanf(fileID,'%c');
-    parserObj = nmeaParser("MessageId","RMC");
-    size(parserObj);
-    rmcData = parserObj(corrections);
-    latitude1 = rmcData(end).Latitude;
-    longitude1 = rmcData(end).Longitude;
+    k = strfind(corrections,"$GNRMC");
+    for i=1:size(k,2)-1
+        singlecorrection=corrections(k(end-i):k(end-i)+68);
+        rmcData = parserObj(singlecorrection);
+        if rmcData.Status == 0
+            latitude1 = rmcData(end).Latitude;
+            longitude1 = rmcData(end).Longitude;
+            break
+        end
+    end
+    
    % gpsTime = rmcData(end).UTCDateTime;
     fclose(fileID);
     fileID = fopen("C:\Users\Skull\Documents\GatorTemp\RTK_GPS\GPS_Test_Parkinglot_Right.ubx",'r');
     corrections=fscanf(fileID,'%c');
-    pnmea = nmeaParser("MessageId","RMC");
-    rmcData = pnmea(corrections);
-    latitude2 = rmcData(end).Latitude;
-    longitude2 = rmcData(end).Longitude; 
+     k = strfind(corrections,"$GNRMC");
+   for i=1:size(k,2)-1
+        singlecorrection=corrections(k(end-i):k(end-i)+68);
+        parserObj = nmeaParser("MessageId","RMC");
+        rmcData = parserObj(singlecorrection);
+        if rmcData.Status == 0
+            latitude2 = rmcData(end).Latitude;
+            longitude2 = rmcData(end).Longitude;
+            break
+        end
+    end
    %  gpsTime = rmcData(end).UTCDateTime;
     fclose(fileID);
     gpsData = struct( ...
