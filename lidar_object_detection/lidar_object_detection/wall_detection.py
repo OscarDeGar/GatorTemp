a419975fc97854a13c1cb04ac7ae9106b899487b
jerWenger <jwenger@olin.edu>
2022-11-08 20:09:03 -0500

    clean this up so charlie and I can do work

--- lidar_object_detection/lidar_object_detection/wall_detection.py
+++ lidar_object_detection/lidar_object_detection/wall_detection.py
@@ -3,23 +3,57 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 import time
-import numpy as Np
+import numpy as np
 import random
+import math
 
 class wall_detection(Node):
     def __init__(self):
+        
+        #build class
         super().__init__("wall_detection")
+
+        #Create subscibers and publishers
         self.sub = self.create_subscription(LaserScan, 'scan', self.injest_scan, 10)
 
-    def injest_scan(self, msg):
+        #allocate variables
+        self.current_scan = None
+        self.scan_length = 761
+        self.front_spread = 15 * math.pi / 180
+        self.danger_threashold = 5
+        
 
+        #self.left_bound = 
+
+    def injest_scan(self, msg):
+        self.current_scan = msg
+        self.check_front_area()
+
+    
+    def check_front_area(self):
+        radians = np.linspace(self.current_scan.angle_min, self.current_scan.angle_max, len(self.current_scan.ranges))
+        mapped_list = list(zip(radians,self.current_scan.ranges))
+        midpoint = math.floor(len(mapped_list) /2)
+
+        print(mapped_list)
+
+
+
+
+
+        
+        
+
+        
+
+        
 
 
 
 
 def main(args=None):
-    rclpy.init(args=args)         # Initialize communication with ROS
-    node = (wall_detection)       # Create our Node
+    rclpy.init()         # Initialize communication with ROS
+    node = wall_detection()       # Create our Node
     rclpy.spin(node)              # Run the Node until ready to shutdown
     rclpy.shutdown()              # cleanup
 
